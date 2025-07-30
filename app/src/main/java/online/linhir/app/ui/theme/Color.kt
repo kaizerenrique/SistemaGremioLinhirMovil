22b9f58b924e957d19c6aea297a8ce9d8ca04ce4
Michael Sandino <68172175+michaelsandino@users.noreply.github.com>
2025-07-30 18:02:46 -0500

    feat (Colors): Definición de paleta de colores

--- app/src/main/java/online/linhir/app/ui/theme/Color.kt
+++ app/src/main/java/online/linhir/app/ui/theme/Color.kt
@@ -2,10 +2,81 @@ package online.linhir.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Esquema de colores para modo claro
+val LightPrimary = Color(0xFF982522)
+val LightOnPrimary = Color(0xFFFFFFFF)
+val LightPrimaryContainer = Color(0xFFFFDAD6)
+val LightOnPrimaryContainer = Color(0xFF410002)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val LightSecondary = Color(0xFFDAA520)
+val LightOnSecondary = Color(0xFF3D2F00)
+val LightSecondaryContainer = Color(0xFFFFE084)
+val LightOnSecondaryContainer = Color(0xFF211A00)
+
+val LightTertiary = Color(0xFF6B5B3D)
+val LightOnTertiary = Color(0xFFFFFFFF)
+val LightTertiaryContainer = Color(0xFFE8DCC6)
+val LightOnTertiaryContainer = Color(0xFF221A08)
+
+val LightError = Color(0xFFBA1A1A)
+val LightOnError = Color(0xFFFFFFFF)
+val LightErrorContainer = Color(0xFFFFDAD6)
+val LightOnErrorContainer = Color(0xFF410002)
+
+val LightBackground = Color(0xFFFFFBFF)
+val LightOnBackground = Color(0xFF1F1A19)
+val LightSurface = Color(0xFFFFFBFF)
+val LightOnSurface = Color(0xFF1F1A19)
+val LightSurfaceVariant = Color(0xFFF5DDD9)
+val LightOnSurfaceVariant = Color(0xFF534341)
+
+val LightOutline = Color(0xFF857370)
+val LightOutlineVariant = Color(0xFFD8C2BD)
+val LightScrim = Color(0xFF000000)
+val LightInverseSurface = Color(0xFF362F2E)
+val LightInverseOnSurface = Color(0xFFFAEEEC)
+val LightInversePrimary = Color(0xFFFFB4AB)
+
+// Esquema de colores para modo oscuro
+val DarkPrimary = Color(0xFFFFB4AB)
+val DarkOnPrimary = Color(0xFF690005)
+val DarkPrimaryContainer = Color(0xFF7F0E0E)
+val DarkOnPrimaryContainer = Color(0xFFFFDAD6)
+
+val DarkSecondary = Color(0xFFE6C547)
+val DarkOnSecondary = Color(0xFF3D2F00)
+val DarkSecondaryContainer = Color(0xFF5A4300)
+val DarkOnSecondaryContainer = Color(0xFFFFE084)
+
+val DarkTertiary = Color(0xFFCCC0AB)
+val DarkOnTertiary = Color(0xFF382F1C)
+val DarkTertiaryContainer = Color(0xFF504531)
+val DarkOnTertiaryContainer = Color(0xFFE8DCC6)
+
+val DarkError = Color(0xFFFFB4AB)
+val DarkOnError = Color(0xFF690005)
+val DarkErrorContainer = Color(0xFF93000A)
+val DarkOnErrorContainer = Color(0xFFFFDAD6)
+
+val DarkBackground = Color(0xFF17120F)
+val DarkOnBackground = Color(0xFFEDE0DD)
+val DarkSurface = Color(0xFF17120F)
+val DarkOnSurface = Color(0xFFEDE0DD)
+val DarkSurfaceVariant = Color(0xFF534341)
+val DarkOnSurfaceVariant = Color(0xFFD8C2BD)
+
+val DarkOutline = Color(0xFFA08C89)
+val DarkOutlineVariant = Color(0xFF534341)
+val DarkScrim = Color(0xFF000000)
+val DarkInverseSurface = Color(0xFFEDE0DD)
+val DarkInverseOnSurface = Color(0xFF362F2E)
+val DarkInversePrimary = Color(0xFF982522)
+
+// Colores personalizados
+val LightSuccess = Color(0xFF2E7D32)
+val LightWarning = Color(0xFFED6C02)
+val LightInfo = Color(0xFF0288D1)
+
+val DarkSuccess = Color(0xFF81C784)
+val DarkWarning = Color(0xFFFFB74D)
+val DarkInfo = Color(0xFF4FC3F7)
