8efac5bbbf02d30028372d3f1c609f5d8585c14a
Michael Sandino <68172175+michaelsandino@users.noreply.github.com>
2025-08-03 17:02:59 -0500

    fix (Drawer Scaffold): remove navigation animation

--- app/src/main/java/online/linhir/app/navigation/AppNavigation.kt
+++ app/src/main/java/online/linhir/app/navigation/AppNavigation.kt
@@ -1,15 +1,6 @@
 package online.linhir.app.navigation
 
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideInHorizontally
-import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,59 +17,29 @@ import online.linhir.app.features.validation.ValidationScreen
 fun AppNavigation(){
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = AppScreens.ValidationScreen.route){
-        materialAnimation(route = AppScreens.ValidationScreen.route){
+        composable(route = AppScreens.ValidationScreen.route){
             ValidationScreen(navController)
         }
-        materialAnimation(route = AppScreens.LoginScreen.route){
+        composable(route = AppScreens.LoginScreen.route){
             LoginScreen(navController)
         }
-        materialAnimation(route = AppScreens.HomeScreen.route){
+        composable(route = AppScreens.HomeScreen.route){
             HomeScreen(navController)
         }
-        materialAnimation(route = AppScreens.MembersScreen.route){
+        composable(route = AppScreens.MembersScreen.route){
             MembersScreen(navController)
         }
-        materialAnimation(route = AppScreens.PaymentsScreen.route){
+        composable(route = AppScreens.PaymentsScreen.route){
             PaymentsScreen(navController)
         }
-        materialAnimation(route = AppScreens.AboutScreen.route){
+        composable(route = AppScreens.AboutScreen.route){
             AboutScreen(navController)
         }
-        materialAnimation(route = AppScreens.NotificationsScreen.route){
+        composable(route = AppScreens.NotificationsScreen.route){
             NotificationsScreen(navController)
         }
-        materialAnimation(route = AppScreens.SettingsScreen.route){
+        composable(route = AppScreens.SettingsScreen.route){
             SettingsScreen(navController)
         }
     }
-}
-
-fun NavGraphBuilder.materialAnimation(route: String, content: @Composable () -> Unit) {
-    composable(
-        route = route,
-        enterTransition = {
-            slideInHorizontally(
-                initialOffsetX = { it },
-                animationSpec = tween(300, easing = FastOutSlowInEasing)
-            ) + fadeIn(tween(300))
-        },
-        exitTransition = {
-            slideOutHorizontally(
-                targetOffsetX = { -it },
-                animationSpec = tween(300, easing = FastOutSlowInEasing)
-            ) + fadeOut(tween(300))
-        },
-        popEnterTransition = {
-            slideInHorizontally(
-                initialOffsetX = { -it },
-                animationSpec = tween(300, easing = FastOutSlowInEasing)
-            ) + fadeIn(tween(300))
-        },
-        popExitTransition = {
-            slideOutHorizontally(
-                targetOffsetX = { it },
-                animationSpec = tween(300, easing = FastOutSlowInEasing)
-            ) + fadeOut(tween(300))
-        }
-    ) { content() }
 }
